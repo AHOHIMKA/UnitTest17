f3050c4f9c1acda58919409253353877d3a2fe13
smirn <smirn@DESKTOP-CH12K9O>
2023-04-04 19:22:33 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject/FileStorageTest.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnitTestEx;
+using Assert = NUnit.Framework.Assert;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// Summary description for FileStorageTest
+    /// </summary>
+    [TestClass]
+    public class FileStorageTest
+    {
+
+        private List<File> files = new List<File>();
+        public const string MAX_SIZE_EXCEPTION = "DIFFERENT MAX SIZE";
+        public const string NULL_FILE_EXCEPTION = "NULL FILE";
+        public const string NO_EXPECTED_EXCEPTION_EXCEPTION = "There is no expected exception";
+        public string filename = "File1";
+        public const string SPACE_STRING = " ";
+        public const string FILE_PATH_STRING = "@D:\\JDK-intellij-downloader-info.txt";
+        public const string CONTENT_STRING = "Some text";
+        public const string REPEATED_STRING = "AA";
+        public const string WRONG_SIZE_CONTENT_STRING = "TEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtextTEXTtext";
+        public const string TIC_TOC_TOE_STRING = "tictoctoe.game";
+
+        public const int NEW_SIZE = 5;
+
+        public FileStorage storage = new FileStorage(NEW_SIZE);
+
+        /* ПРОВАЙДЕРЫ */
+
+        static object[] NewFilesData =
+        {
+            new object[] { new File(REPEATED_STRING, CONTENT_STRING) },
+            new object[] { new File(SPACE_STRING, WRONG_SIZE_CONTENT_STRING) },
+            new object[] { new File(FILE_PATH_STRING, CONTENT_STRING) }
+        };
+
+        static object[] FilesForDeleteData =
+        {
+            new object[] { new File(REPEATED_STRING, CONTENT_STRING), REPEATED_STRING },
+            new object[] { null, TIC_TOC_TOE_STRING }
+        };
+
+        static object[] NewExceptionFileData = {
+            new object[] { new File(REPEATED_STRING, CONTENT_STRING) }
+        };
+
+        /* Тестирование записи файла */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void WriteTest()  // Убрал параметр из метода, из-за него не работал тест
+        {
+            File file1 = new File("test.png"); //Параметр создал новый
+
+            Assert.True(storage.Write(file1));
+            storage.DeleteAllFiles(); //Для возврата в начальное состояние
+        }
+
+        /* Тестирование записи дублирующегося файла */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void WriteExceptionTest() // Убрал параметр из метода, из-за него не работал тест
+        {
+            File file1 = new File("test.png"); //Параметр создал новый вручную, имитация создания файла
+
+            bool isException = false;
+            try
+            {
+                storage.Write(file1);
+                Assert.False(storage.Write(file1));
+                storage.DeleteAllFiles(); // Очистка storage, возврат в начальное положение 
+            }
+            catch (FileNameAlreadyExistsException)
+            {
+                isException = true;
+            }
+            Assert.True(isException, NO_EXPECTED_EXCEPTION_EXCEPTION);
+        }
+
+        /* Тестирование проверки существования файла */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void IsExistsTest() // Убрал параметр из метода, из-за них не работал тест
+        {
+            //Создается объект File file с данными.
+            //  проверка, существует ли файл в хранилище до записи(ожидаемое значение - false).
+            //   запись файла в хранилище.
+            //проверка, существует ли файл в хранилище после записи(ожидаемое значение - true).
+            //и очистка хранилища
+
+            // Storage изначально пустой
+
+            List<File> files = new List<File>();
+            File file1 = new File("test.png"); //Параметр создал новый вручную, имитация создания файла
+
+            // Act
+            bool isFileExistsBeforeWrite = storage.IsExists(file1.GetFilename());
+
+            storage.Write(file1); //Заполнение хранилища(Изначально был пустой)
+
+            bool isFileExistsAfterWrite = storage.IsExists(file1.GetFilename());
+
+            // Assert
+            Assert.False(isFileExistsBeforeWrite);
+            Assert.True(isFileExistsAfterWrite);
+
+            // Cleanup
+            storage.DeleteAllFiles(); //Для возврата в начальное состояние
+
+        }
+
+        /* Тестирование удаления файла */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void DeleteTest(/*File file, String fileName*/) // Убрал параметр из метода, из-за них не работал тест
+        {
+
+            files.Clear();
+            Assert.AreEqual(files.Count, 0); //проверка на очистку List
+
+        }
+        public string GetFilename()
+        {
+            return filename;
+        }
+
+
+        /* Тестирование получения файлов */
+        [TestMethod]
+        public void GetFilesTest() //не изменял,только подписал [TestMethod]
+        {
+            foreach (File el in storage.GetFiles())
+            {
+                Assert.NotNull(el);
+            }
+        }
+
+
+        /* Тестирование получения файла */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void GetFileTest() // Убрал параметр из метода, из-за них не работал тест
+        {
+            File expectedFile = new File("filename.txt"); //имитация сохранения файла
+
+            // Создаем файл и записываем его в хранилище
+            storage.Write(expectedFile);
+
+            // Получаем файл из хранилища и сравниваем с ожидаемым файлом
+            File actualFile = storage.GetFile(expectedFile.GetFilename());
+            bool difference = actualFile.GetFilename().Equals(expectedFile.GetFilename()) && actualFile.GetSize().Equals(expectedFile.GetSize());
+
+            // Проверяем, что файлы равны
+            Assert.IsTrue(difference, string.Format("There are some differences in {0} or {1}", expectedFile.GetFilename(), expectedFile.GetSize()));
+        }
+
+        public List<File> GetFiles() // Получение файлов
+        {
+            return files;
+        }
+
+
+        public File GetFile(String fileName) //Получение файла/проверка на существование/ Проверка имени
+        {
+            if (IsExists(fileName))
+            {
+                foreach (File file in files)
+                {
+                    if (file.GetFilename().Contains(fileName))
+                    {
+                        return file;
+                    }
+                }
+            }
+            return null;
+        }
+        public bool IsExists(String fileName) // Проверка на существание 
+        {
+            // Для каждого элемента с типом File из Листа files
+            foreach (File file in files)
+            {
+                // Проверка имени
+                if (file.GetFilename().Contains(fileName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ UnitTestProject/FileTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using UnitTestEx;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class FileTest
+    {
+        public string filename = "TestFile1";
+        public const string SIZE_EXCEPTION = "Wrong size";
+        public const string NAME_EXCEPTION = "Wrong name";
+        public const string SPACE_STRING = " ";
+        public const string FILE_PATH_STRING = "@D:\\JDK-intellij-downloader-info.txt";
+        public const string WRONG_FILE_NAME = "example_file_name_not_be_allowed";
+        public const string CONTENT_STRING = "Some text";
+        public double lenght;
+
+        /* ПРОВАЙДЕР */
+        static object[] FilesData =
+        {
+            new object[] {new File(FILE_PATH_STRING, CONTENT_STRING), FILE_PATH_STRING, CONTENT_STRING},
+            new object[] { new File(SPACE_STRING, SPACE_STRING), SPACE_STRING, SPACE_STRING}
+        };
+
+        /* Тестируем получение размера */
+        [TestMethod] // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска и предотвращения ошибки datarow
+        public void GetSizeTest() // Убрал все параметры
+        {
+
+            string content = CONTENT_STRING; // Добавл в переменную рандомный текст
+            lenght = content.Length / 2;
+            Assert.AreEqual(lenght, content.Length / 2);
+
+        }
+
+        /* Тестируем получение имени - если имени нет, то выйдет ошибка */
+        [TestMethod]  // Исправил это - [Test, TestCaseSource(nameof(NewFilesData))] на тест-метод для запуска
+        public void GetFilenameTest()
+        {
+            String name = "TestFile1"; // Создал переменную имя файла
+            Assert.AreEqual(GetFilename(), name, NAME_EXCEPTION);
+        }
+        public string GetFilename() // Получить имя файла - например для сверки имен
+        {
+            return filename;
+        }
+
+
+        [TestMethod]
+        public void FileNameShouldNotExceed32Characters()
+        {                                           //Тест №1 - проверка того
+                                                    // что длина названия файла не превышает 32 символа
+            string fileName = WRONG_FILE_NAME;
+            Assert.LessOrEqual(fileName.Length, 32); //код дял проверки, что файл не превышает 32 симовловв названии
+        }
+
+        [TestMethod]
+        public void TestTextFileExtension()
+        {                                       //Тест №2 - проверка того
+                                                // что у файла расширение txt, а не другое иное
+            // Arrange
+            string filePath = FILE_PATH_STRING;     //Пример файла с расширением .txt
+            // Act
+            string fileExtension = System.IO.Path.GetExtension(filePath); // Получаем его расширение 
+
+            // Assert
+            Assert.AreEqual(".txt", fileExtension, "File extension is not '.txt'"); //Сравниваем, что если расширение будет
+                                                                                    //не txt, то будет ошибка и тест провален
+        }
+
+        [TestMethod]
+        public void GetFilenameTestIfSymbols() 
+        {
+            //Тест №3 - проверка того что
+            // в имени файла не содержаться специальные символы
+
+            GetFilename();                  //Получение имени файла
+            bool haveSymbolsInNameFile = false;
+            if (GetFilename().Contains("!,/,*,@,$,%,^,>,&"))
+            {
+                haveSymbolsInNameFile = true;
+                Assert.IsTrue(haveSymbolsInNameFile, NAME_EXCEPTION);       //Проверка того,что нет спец символов
+            }
+            else
+            {
+                Assert.AreEqual(GetFilename(), "TestFile1"); //Вызов ошибки если надены спец символы
+            }
+        }
+
+
+    }
+}
